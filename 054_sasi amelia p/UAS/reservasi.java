53faf8de4a4f2b4558eaddf3473ee38c5768920f
Sasiaptr <ptrsasi077@gmail.com>
2026-07-01 19:03:53 +0700

    Create reservasi.java

---
+++ 054_sasi amelia p/UAS/reservasi.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Reservasi {
+   private String bookingId;
+   private String nim;
+   private String namaRuang;
+  
+   public Reservasi(String bookingId, String nim, String namaRuang) {
+     this.bookingId = bookingId;
+     this.nim = nim;
+     this.namaRuang = namaRuang;
+     }
+  
+    // Getter untuk validasi
+    public String getBookingId() { return bookingId; }
+    public String getNim() { return nim; }
+    public String getNamaRuang() { return namaRuang; }
